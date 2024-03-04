9859f65140fbd4801fdb66d617a0147c2d8d5fa1
mbarnesdev <martinbarnes.dev@gmail.com>
2024-03-04 19:36:27 +0000

    format file + formatting

--- wezterm.lua
+++ wezterm.lua
@@ -1,10 +1,10 @@
-require("math")
+require "math"
 
-local wezterm = require("wezterm")
+local wezterm = require "wezterm"
 
 local config = {}
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 local color_scheme = "Batman"
@@ -14,23 +14,23 @@ local font_weight = "Medium"
 
 config.color_scheme = color_scheme
 config.background = {
-	{
-		source = {
-			File = os.getenv("HOME") .. "/.config/wezterm/background.jpeg",
-		},
-		hsb = {
-			brightness = 0.15,
-		},
-	},
+  {
+    source = {
+      File = os.getenv "HOME" .. "/.config/wezterm/background.jpeg",
+    },
+    hsb = {
+      brightness = 0.15,
+    },
+  },
 }
 config.font = wezterm.font(font, { weight = font_weight })
 config.font_size = font_size
 config.use_fancy_tab_bar = false
 config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
 }
 
 return config
